e40b805f114d98e7826ba8d26788be5ebd3391f5
Sam Allen <samallen659@gmail.com>
2023-05-23 19:48:19 +0100

    added userRouter to app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import { userRouter } from "./routers/user";
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV === "Development") {
 }
 
 app.use(cors());
+
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
 	res.send("Welcome to the Samco shop");
