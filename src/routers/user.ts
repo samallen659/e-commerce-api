895c7a03a41d7c4461f123b667871e66eb006996
Sam Allen <samallen659@gmail.com>
2023-05-25 20:15:27 +0100

    refactored register route for password validation

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -2,21 +2,28 @@ import express from "express";
 import { User } from "../types";
 import * as argon2 from "argon2";
 import { createUser } from "../db/user";
+import { hashPassword, isPasswordStrong } from "../auth/password";
 
 export const userRouter = express.Router();
-const ARGON2_SECRET = process.env.ARGON2_SECRET as string;
 
 userRouter.post("/register", async (req, res) => {
-	const { firstName, lastName, email } = req.body;
-	const user = { firstName, lastName, email, isAdmin: false } as User;
-	const password = await argon2.hash(req.body.password, { secret: Buffer.from(ARGON2_SECRET) });
-	const createdUser = await createUser(user, password);
-	res.status(201).json({
-		message: "User created",
-		User: {
-			firstName: createdUser.firstName,
-			lastName: createdUser.lastName,
-			email: createdUser.email,
-		},
-	});
+	const password = req.body.password;
+	if (isPasswordStrong(password)) {
+		const { firstName, lastName, email } = req.body;
+		const user = { firstName, lastName, email, isAdmin: false } as User;
+		const passwordHash = await hashPassword(password);
+		const createdUser = await createUser(user, passwordHash);
+		res.status(201).json({
+			message: "User created",
+			User: {
+				firstName: createdUser.firstName,
+				lastName: createdUser.lastName,
+				email: createdUser.email,
+			},
+		});
+	} else {
+		res.status(400).json({
+			message: "Password does not meet the security requirements",
+		});
+	}
 });
