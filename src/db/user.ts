211a1e851b01a9c17abeb5b05420cad207c5cb69
Sam Allen <samallen659@gmail.com>
2023-05-25 19:04:26 +0100

    created db connection and user functions

---
+++ src/db/user.ts
@@ -0,0 +1,52 @@
+import { db } from "./db";
+import { User } from "../types";
+
+async function createUser(user: User, password: string): Promise<User> {
+	return await db.user.create({
+		data: {
+			firstName: user.firstName,
+			lastName: user.lastName,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			password: password,
+		},
+	});
+}
+
+async function getUser(email: string): Promise<User | null> {
+	return await db.user.findFirst({
+		where: {
+			email: email,
+		},
+		select: {
+			firstName: true,
+			lastName: true,
+			email: true,
+			isAdmin: true,
+		},
+	});
+}
+
+async function getUserPassword(email: string): Promise<{ password: string } | null> {
+	return await db.user.findFirst({
+		where: {
+			email: email,
+		},
+		select: {
+			password: true,
+		},
+	});
+}
+
+async function getAllUsers(): Promise<User[] | null> {
+	return await db.user.findMany({
+		select: {
+			firstName: true,
+			lastName: true,
+			email: true,
+			isAdmin: true,
+		},
+	});
+}
+
+export { createUser, getUser, getUserPassword, getAllUsers };
