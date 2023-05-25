709b96a2752ba0d4e4ed6a2512f98f6cbe2acc23
Sam Allen <samallen659@gmail.com>
2023-05-25 19:56:05 +0100

    register route adding user to database

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "Development") {
 }
 
 app.use(cors());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/user", userRouter);
--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,2 +1,22 @@
 import express from "express";
+import { User } from "../types";
+import * as argon2 from "argon2";
+import { createUser } from "../db/user";
+
 export const userRouter = express.Router();
+const ARGON2_SECRET = process.env.ARGON2_SECRET as string;
+
+userRouter.post("/register", async (req, res) => {
+	const { firstName, lastName, email } = req.body;
+	const user = { firstName, lastName, email, isAdmin: false } as User;
+	const password = await argon2.hash(req.body.password, { secret: Buffer.from(ARGON2_SECRET) });
+	const createdUser = await createUser(user, password);
+	res.status(201).json({
+		message: "User created",
+		User: {
+			firstName: createdUser.firstName,
+			lastName: createdUser.lastName,
+			email: createdUser.email,
+		},
+	});
+});
